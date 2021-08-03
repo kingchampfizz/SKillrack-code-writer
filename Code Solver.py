74419a11f9b055e592e28eb8a12f3097e89e8004
kingchampfizz <88394100+kingchampfizz@users.noreply.github.com>
2021-08-03 21:43:43 +0530

    Delete Code Solver.py

--- Code Solver.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(20)
-with open("Enter the path for your text file that contains the code") as f:
-   text = f.read()
-
-pyautogui.write(text,interval=0.01)
-
